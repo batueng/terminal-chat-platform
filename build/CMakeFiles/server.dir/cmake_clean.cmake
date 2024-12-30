7aa7aa64f03b75bb7ab40c929da4a5407eb34343
Jake Zur <jakezur@umich.edu>
2024-12-30 15:01:56 -0500

    username sending in progress, something worng withour socket send and rcv

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/MessageServer.cpp.o"
+  "CMakeFiles/server.dir/MessageServer.cpp.o.d"
+  "CMakeFiles/server.dir/ServerSocket.cpp.o"
+  "CMakeFiles/server.dir/ServerSocket.cpp.o.d"
+  "CMakeFiles/server.dir/Session.cpp.o"
+  "CMakeFiles/server.dir/Session.cpp.o.d"
+  "CMakeFiles/server.dir/UserSocket.cpp.o"
+  "CMakeFiles/server.dir/UserSocket.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/tcp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp.dir/Client.cpp.o"
+  "CMakeFiles/tcp.dir/Client.cpp.o.d"
+  "CMakeFiles/tcp.dir/ClientSocket.cpp.o"
+  "CMakeFiles/tcp.dir/ClientSocket.cpp.o.d"
+  "CMakeFiles/tcp.dir/graphics.cpp.o"
+  "CMakeFiles/tcp.dir/graphics.cpp.o.d"
+  "tcp"
+  "tcp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
