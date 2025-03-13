2cc9835b6d593dd1375b602404921fcfe53806f5
Batu Engin <batueng@umich.edu>
2025-03-12 23:45:22 -0400

    tRevert "change"
    This reverts commit 4918ba93e34ad9c167a8cf53738cae80d99fed82.

--- Client.cpp
+++ Client.cpp
@@ -17,7 +17,9 @@ Client *Client::instance = nullptr;
 
 Client::Client(std::string &_server_ip, int _server_port,
                std::string debug_file)
-    : req_handler(_server_ip, _server_port), fout(debug_file) {
+    : req_handler(_server_ip, _server_port),
+      fout(std::to_string((unsigned long long)(void **)this) + "_" +
+           debug_file) {
   initscr();
   use_default_colors();
   start_color();
@@ -222,6 +224,7 @@ void Client::print_home_screen() {
             boost::unique_lock<boost::mutex> lock(sess_mtx);
             c = _c;
             curr_sess = arg;
+            fout << curr_sess << std::endl;
           }
 
           delwin(home_win);
